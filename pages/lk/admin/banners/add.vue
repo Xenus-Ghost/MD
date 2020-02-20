3455831d353db793bbc53632a846307d4e068173
Sergey <ser12563208@yandex.ru>
2020-02-20 09:31:04 +0400

    категории, чпу...

---
+++ pages/lk/admin/banners/add.vue
@@ -0,0 +1,11 @@
+<template>
+  <form action=""></form>
+</template>
+
+<script>
+export default {
+  name: 'Add'
+}
+</script>
+
+<style scoped></style>
---
+++ pages/lk/admin/banners/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="banner-admin">
+    <ul class="banners">
+      <li v-for="(banner, i) in banners" :key="i" class="banners"></li>
+    </ul>
+    <div v-if="errors" class="error">Произошла ошибка</div>
+    <Button>Добавить</Button>
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+import { getUrl } from '@/assets/js/util'
+
+export default {
+  name: 'BannerAd',
+  async asyncData(context) {
+    let banners = null
+    const errors = []
+    await context.$axios
+      .get(getUrl('/admin/banners'))
+      .then((response) => (banners = response.data))
+      .catch((e) => errors.push(e.response.data))
+    return { banners, errors }
+  },
+  data() {
+    return {
+      banners: null,
+      errors: null
+    }
+  },
+  created() {
+    console.log(getUrl('admin/banners'))
+  }
+}
+</script>
+
+<style scoped></style>
